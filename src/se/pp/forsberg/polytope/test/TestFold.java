bd0b1e71fa7d45aa663806e79c4c534875de2a66
kaewberg <Johan.Kaewb.Forsberg@consult.nordea.com>
2016-04-19 11:53:31 +0200

    n-d test fold

--- src/se/pp/forsberg/polytope/test/TestFold.java
+++ src/se/pp/forsberg/polytope/test/TestFold.java
@@ -9,28 +9,43 @@ public class TestFold {
   
   
   public static void main(String[] arguments) {
-    System.out.println("Tetrahedron, 70.528779 degrees");
-    fold3(Math.PI/3, Math.PI/3, Math.PI/3);
     System.out.println("Cube, 90 degrees");
-    fold3(Math.PI/2, Math.PI/2, Math.PI/2);
-    System.out.println("Triangular prism");
-    fold3(Math.PI/2, Math.PI/3, Math.PI/2);
-    System.out.println("Triangle square pentagon");
-    fold3(Math.PI/3, Math.PI/2, 3*Math.PI/5);
+    fold3(PI/2, PI/2, PI/2);
+    System.out.println("\nTriangular prism");
+    fold3(PI/2, PI/3, PI/2);
+    System.out.println("\nTriangle square pentagon");
+    fold3(PI/3, PI/2, 3*PI/5);
+    
+    System.out.println("\nTetrahedron (3-simplex), 70.528779 degrees");
+    fold3(PI/3, PI/3, PI/3);
+    System.out.println("\n5-cell (4-simplex), " + a(acos(1.0/4)));
+    fold3(acos(1.0/3), acos(1.0/3), acos(1.0/3));
+    System.out.println("\n5-simplex, " + a(acos(1.0/5)));
+    fold3(acos(1.0/4), acos(1.0/4), acos(1.0/4));
+    System.out.println("\n6-simplex, " + a(acos(1.0/6)));
+    fold3(acos(1.0/5), acos(1.0/5), acos(1.0/5));
   }
 
   private static void fold3(double v1, double v2, double v3) {
-    double cv1 = Math.cos(v1), cv2 = Math.cos(v2), cv3 = Math.cos(v3);
-    double sv1 = Math.sin(v1), sv2 = Math.sin(v2), sv3 = Math.sin(v3);
+    double cv1 = cos(v1), cv2 = cos(v2), cv3 = cos(v3);
+    double sv1 = sin(v1), sv2 = sin(v2), sv3 = sin(v3);
     
-    double f1f2 = acos((cv3 - cv1*cv2)/(sv1*sv2));
-    double f2f3 = acos((cv1 - cv2*cv3)/(sv2*sv3));
-    double f3f1 = acos((cv2 - cv3*cv1)/(sv3*sv1));
+    double p1p2 = acos((cv3 - cv1*cv2)/(sv1*sv2));
+    double p2p3 = acos((cv1 - cv2*cv3)/(sv2*sv3));
+    double p3p1 = acos((cv2 - cv3*cv1)/(sv3*sv1));
     
-    System.out.println("Fold polygons f1, f2, f3 with angles " + a(v1) + ", " + a(v2) + ", " + a(v3) + " around a point -> dihedral angles ");
-    System.out.println("  f1f2 = "+ a(f1f2));
-    System.out.println("  f2f3 = "+ a(f2f3));
-    System.out.println("  f3f1 = "+ a(f3f1));
+    System.out.println("Fold polytopes p1, p2, p3 with angles " + a(v1) + ", " + a(v2) + ", " + a(v3) + " around a common sub-ridge -> dihedral angles ");
+    System.out.println("  p1p2 = "+ a(p1p2));
+    System.out.println("  p2p3 = "+ a(p2p3));
+    System.out.println("  p3p1 = "+ a(p3p1));
+    
+    // If all angles are common
+    // For instance tetrahedron
+    // v2 = acos((cos(v) - cos^2(v))/sin^2(v));
+    // Reused in 4d
+    // v3 = acos((cos(v2) - cos^2(v2))/sin^2(v2));
+    // v3 = acos((cos(acos((cos(v) - cos^2(v))/sin^2(v))) - cos^2(acos((cos(v) - cos^2(v))/sin^2(v))))/sin^2(acos((cos(v) - cos^2(v))/sin^2(v))));
+    // v3 = acos(((cos(v) - cos^2(v))*csc^2(v) - (cos(v) - cos^2(v))^2*csc^4(v)) / (1 - (cos(v) - cos^2(v))^2*csc^4(v)))
   }
 
   private static NumberFormat numberFormat = new DecimalFormat("###.#°");
